efbe5ed431c3ded4ab0216a24edb1a4cbf605329
PseudoTorque <shreyastyagi10@gmail.com>
2024-12-19 19:44:35 +0530

    First commit.

---
+++ main/src/utils.py
@@ -0,0 +1,119 @@
+"""
+
+Utility functions for the WebsiteTopicDetection module.
+
+"""
+
+from string import punctuation
+from requests import get
+from bs4 import BeautifulSoup
+from nltk.corpus import stopwords
+from spacy import load
+#from spacy.cli import download
+
+#download("en_core_web_sm")
+NLP = load("en_core_web_sm")
+
+def get_bytes_from_url(url: str, timeout: int = 10) -> bytes:
+
+    """
+    Get the bytes from a URL.
+
+    Args:
+        url (str): The URL to get the bytes from.
+        timeout (int, optional): The timeout for the request. Defaults to 10.
+
+    Returns:
+        bytes: The bytes from the URL.
+    """
+
+    response = get(url, timeout=timeout)
+
+    response.raise_for_status()
+
+    return response.content
+
+def get_hyperlinks_from_page_bytes(page_bytes: bytes) -> list[str]:
+
+    """
+    Get the hyperlinks from a page.
+
+    Args:
+        page_bytes (bytes): The bytes of the page.
+
+    Returns:
+        list[str]: The hyperlinks from the page.
+    """
+
+    soup = BeautifulSoup(page_bytes, "html.parser")
+
+    hyperlinks = []
+
+    link: BeautifulSoup = None
+
+    for link in soup.find_all("a"):
+        hyperlinks.append(link.get("href", default=None))
+
+    return [i for i in hyperlinks if i is not None]
+
+def clean_html(page_bytes: bytes) -> str:
+
+    """
+    Clean the HTML of a page (in bytes).
+
+    Args:
+        page_bytes (bytes): The bytes of the page.
+
+    Returns:
+        str: The cleaned HTML.
+    """
+
+    soup = BeautifulSoup(page_bytes, "html.parser")
+
+    for data in soup(['style', 'script', 'code', 'a']):
+
+        data.decompose()
+
+    return ' '.join(soup.stripped_strings)
+
+test = get_bytes_from_url("https://pulse.zerodha.com")
+
+def clean_text(text: str) -> str:
+
+    """
+    Clean the text of a page (convert to lowercase, remove stopwords, remove punctuation).
+
+    Args:
+        text (str): The text to clean.
+
+    Returns:
+        str: The cleaned text.
+    """
+
+    #convert text to lowercase
+    text = text.lower()
+
+    #remove newlines
+    text = text.replace("\n", " ")
+
+    #remove punctuation
+    translator = str.maketrans('', '', punctuation)
+    text = text.translate(translator)
+
+    #remove stopwords
+    temp_stopwords = set(stopwords.words("english"))
+    text = [word for word in text.split() if word not in temp_stopwords]
+
+    #remove whitespace
+    text = [word for word in text if word.strip()]
+
+    #NLP using spacy
+    doc = NLP(" ".join(text))
+    text = [token.text for token in doc if not token.is_punct]
+
+    #join the text
+    return " ".join(text)
+
+test = get_bytes_from_url("https://pulse.zerodha.com")
+#print(clean_html(test))
+print(clean_text(clean_html(test)))
